129fbb4320a4af0ae0e46251057fec9fbb280a3e
cpotella <cpotella@gmail.com>
2025-03-29 13:37:40 -0300

    implementar hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <p>
-         Prueba branch
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routes/AppRouter";
 import { CartProvider } from "./context/CarContext";
-import "../src/index.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
